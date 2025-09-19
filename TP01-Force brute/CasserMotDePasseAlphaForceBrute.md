09aeebf1f39fd234ead825bd8c810029c90d7227
Lafouc1 <nathfoucault17220@gmail.com>
2025-09-19 14:42:34 +0200

    Add files via upload

---
+++ TP01-Force brute/CasserMotDePasseAlphaForceBrute.md
@@ -0,0 +1,26 @@
+#1. Table ASCII
+1) ASCII 97 correspond au caractère "a".
+2) Il s'affiche dans la console le caractère correponsant au code ASCII 97.
+---
+#2. Création d'un mot de passe
+
+3) Il s'affiche dans la console les caractères correpspondant à chacun des codes ASCII.
+
+---
+#3. Afficher toutes les lettres minuscules
+
+4) 
+![alt text](code_afficher_lettres_minuscules.png)
+
+---
+#4. Générer un mot de passe d'une lettre et la retrouver
+
+5)
+![alt text](retrouver_lettre.png)
+
+---
+#5. Idem que précédemment mais avec 2 lettres
+
+6)
+
+![alt text](retrouver_2_lettres.png)
---
+++ TP01-Force brute/CasserMotDePasseAlphaForceBrute/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CasserMotDePasseAlphaForceBrute
+{
+   class Program
+   {
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            // génère un code ASCII entre 97 et 122
+            int codeASCIIHasard1 = rnd.Next(97, 122);
+            int codeASCIIHasard2 = rnd.Next(97, 122);
+
+            // obtient la lettre aléatoire à partir du code ASCII
+            char lettreHasard1 = (char)codeASCIIHasard1;
+            char lettreHasard2 = (char)codeASCIIHasard2;
+            string motDePasse = lettreHasard1.ToString() + lettreHasard2.ToString();
+
+
+            for (int i = 97; i < 122; i++)
+            {
+                char lettreHasardForceBrute1 = (char)i;
+                if (lettreHasardForceBrute1 == lettreHasard1)
+                  {
+                    Console.WriteLine("Première lettre trouvée : " + lettreHasardForceBrute1);
+
+                    for (int j = 97; j < 122; j++)
+                    {
+                        char lettreHasardForceBrute2 = (char)j;
+                        if (lettreHasardForceBrute2 == lettreHasard2)
+                        {
+                            Console.WriteLine("Deuxième lettre trouvée : " + lettreHasardForceBrute2);
+                            Console.WriteLine("Mot de passe trouvé : " + motDePasse);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
